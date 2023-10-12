b72c9a0c85c3cfabde7326987a20530ffe16add3
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2023-10-12 17:21:26 +0200

    initial commit

---
+++ src/main/java/pl/akademiaspecjalistowit/PackageLifecycleProcessor/PackageLifecycleProcessorApplication.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.PackageLifecycleProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageLifecycleProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageLifecycleProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/akademiaspecjalistowit/PackageLifecycleProcessor/PackageLifecycleProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiaspecjalistowit.PackageLifecycleProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageLifecycleProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
